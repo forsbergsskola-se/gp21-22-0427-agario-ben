5b84a4851a450a174c802639fd3ed7316ff5ebb3
Benjamin Ting <tingzhb@gmail.com>
2022-06-09 19:51:27 +0200

    Add UDP server.
    Retry with better connection

--- OpenWord-MMO/Program.cs
+++ OpenWord-MMO/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var serverEndPoint = new IPEndPoint(IPAddress.Loopback, 1138);
+
+var server = new UdpClient(serverEndPoint);
+
+while (true) {
+	IPEndPoint clientEndpoint = default;
+	var clientData = server.Receive(ref serverEndPoint);
+	var clientMessage = Encoding.ASCII.GetString(clientData);
+
+	Console.WriteLine($"Packet received from: {clientEndpoint} saying: {clientMessage}");
+
+	if (clientData.Length is 0 or > 20 || clientMessage.Any(char.IsWhiteSpace)) {
+		var exception = Encoding.ASCII.GetBytes("Whoops! Message is either over 20 characters or contains spaces. Try again!");
+		server.Send(exception, clientEndpoint);
+	}
+	else {
+		Console.WriteLine("Message Accepted!");
+		var message = clientMessage + " ";
+		var serverMessageData = Encoding.ASCII.GetBytes(message);
+		server.Send(serverMessageData, clientEndpoint);
+	}
+}
